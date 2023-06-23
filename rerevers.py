0c1e272086de5142058fe58113cd39d55911e899
Neonn7528 <Neonn7@yandex.ru>
2023-06-23 17:13:37 +0300

    метод разворота связного списка

--- rerevers.py
+++ rerevers.py
@@ -3,10 +3,11 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
-    
+
     def reverse(self):
         prev = None
         another = self.head
@@ -16,17 +17,18 @@ class LinkedList:
             prev = another
             another = next
         self.head = prev
-    
+
     def add_node(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
-    
+
     def print_list(self):
         node = self.head
         while node:
-            print(node.data,end=" ")
+            print(node.data, end=" ")
             node = node.next
+
 
 # создаем экземпляр связного списка
 my_list = LinkedList()
